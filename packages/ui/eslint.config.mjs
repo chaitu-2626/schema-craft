51c86b3df17a5d42c1208cf9e28665647d2aee9b
chaitu-2626 <m.vinaychaitanya@gmail.com>
2025-04-27 18:19:22 +0530

    Moved the eslint and typescript folder into config folder for better organization

--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -1,4 +1,4 @@
-import { config } from "@repo/eslint-config/react-internal";
+import { config } from "@schema-craft/eslint-config/react-internal";
 
 /** @type {import("eslint").Linter.Config} */
 export default config;
