a996186b5c19f51781d2476a96eda4397886c8c9
chaitu-2626 <m.vinaychaitanya@gmail.com>
2025-05-03 19:20:23 +0530

    Updated Seed data

--- packages/db/src/seed/data/index.ts
+++ packages/db/src/seed/data/index.ts
@@ -1,31 +1,23 @@
-export const erdAttributeJson = (await import('./erdAttributes.json', {
-    assert: { type: 'json' }
-})).default;
+import {
+    ErdAttributeTypes,
+    ErdDiagramTypes,
+    ErdEntityTypes,
+    ErdRelationshipTypes,
+    FavoriteTypes,
+    TeamMemberTypes,
+    TeamTypes,
+    UserTypes,
+} from '@types';
 
-export const erdDiagramJson = (await import('./erdDiagrams.json', {
-    assert: { type: 'json' }
-})).default;
+async function loadJson<T = unknown>(path: string): Promise<T> {
+    return (await import(path, { assert: { type: 'json' } })).default;
+}
 
-export const erdEntityJson = (await import('./erdEntities.json', {
-    assert: { type: 'json' }
-})).default;
-
-export const erdRelationshipJson = (await import('./erdRelationships.json', {
-    assert: { type: 'json' }
-})).default;
-
-export const favoriteJson = (await import('./favorites.json', {
-    assert: { type: 'json' }
-})).default;
-
-export const teamMemberJson = (await import('./teamMembers.json', {
-    assert: { type: 'json' }
-})).default;
-
-export const teamJson = (await import('./teams.json', {
-    assert: { type: 'json' }
-})).default;
-
-export const userJson = (await import('./users.json', {
-    assert: { type: 'json' }
-})).default;
+export const erdAttributeData = await loadJson<ErdAttributeTypes.CreateErdAttribute[]>('./erdAttributes.json');
+export const erdDiagramData = await loadJson<ErdDiagramTypes.CreateErdDiagram[]>('./erdDiagrams.json');
+export const erdEntityData = await loadJson<ErdEntityTypes.CreateErdEntity[]>('./erdEntities.json');
+export const erdRelationshipData = await loadJson<ErdRelationshipTypes.CreateErdRelationship[]>('./erdRelationships.json');
+export const favoriteData = await loadJson<FavoriteTypes.CreateFavorite[]>('./favorites.json');
+export const teamMemberData = await loadJson<TeamMemberTypes.CreateTeamMember[]>('./teamMembers.json');
+export const teamData = await loadJson<TeamTypes.CreateTeam[]>('./teams.json');
+export const userData = await loadJson<UserTypes.CreateUserInput[]>('./users.json');
