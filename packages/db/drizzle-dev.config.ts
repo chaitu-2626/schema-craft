755202992aecf4b28f82370571e59ef4e4280be8
chaitu-2626 <m.vinaychaitanya@gmail.com>
2025-05-03 19:24:52 +0530

    seperated drizzle config for dev and production mode

--- packages/db/drizzle-dev.config.ts
+++ packages/db/drizzle-dev.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "drizzle-kit";
-import env from "@env";
+import {env} from "@config";
 
 export default defineConfig({
     schema: "./dist/src/schema/index.js",
-    out: "./src/migration",
+    out: "./src/migrations",
     dialect: "postgresql",
     dbCredentials: {
         url: env.DATABASE_URL,
